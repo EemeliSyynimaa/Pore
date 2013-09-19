24773ec1cb1a6bf52061051f8e35dfbe340d91c0
eeneku <eeneku@gmail.com>
2013-09-19 21:16:34 +0300

    Forgot to include the new files. Heh.

---
+++ engine/resource_manager.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import yaml
+import pyglet
+
+class ResourceManager(object):
+    """ Class for managing resources. """
+    def __init__(self):
+        self.data = {}
+
+    def load_image(self, id, image, location=None):
+        if location:
+            temp_loader = pyglet.resource.Loader(location)
+            temp = temp_loader.image(image)
+        else:
+            temp = pyglet.resource.image(image)
+
+        self.data[id] = temp
+
+    def load_media(self, id, media, location=None):
+        if location:
+            temp_loader = pyglet.resource.Loader(location)
+            temp = temp_loader.media(media)
+        else:
+            temp = pyglet.resource.media(media)
+
+        self.data[id] = temp
+
+    def load_yaml(self, id, file):
+        pass
